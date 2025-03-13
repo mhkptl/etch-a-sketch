1f09b54abf968034b2e89a116df520e55db3b55a
Mahek Patel <me@mahekpatel.ca>
2025-03-12 20:33:03 -0400

    Change color on hover and size with button prompt

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
 const container = document.querySelector("#container");
+const sizeBtn = document.querySelector("#btn");
 
-for (let row = 0; row <= 16; row++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("gridRow");
 
-    for (let col = 0; col <= 16; col++){
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("gridSquare");
-        rowDiv.appendChild(squareDiv);
-    }
+function sideSize() {
 
-    container.appendChild(rowDiv);
+    let size = prompt("Enter a side length");
+        
+        if (size > 100) {
+            size = 100;
+        }
+        else if (size < 1) {
+            size = 1;
+        }
+
+    return size;
 }
+
+function createGrid() {
+    
+    let size = sideSize();
+
+    // clear the previous grid: while the container has a firstChild, it will be deleted
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+    // create grid
+    for (let row = 0; row < size; row++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("gridRow");
+
+        for (let col = 0; col < size; col++){
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("gridSquare");
+            rowDiv.appendChild(squareDiv);
+        }
+
+        container.appendChild(rowDiv);
+    }
+    
+    // change color on hover
+    const squares = document.querySelectorAll(".gridSquare");
+
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", (event) => {event.target.style.backgroundColor = "pink";});
+    });
+
+}
+
+//sizeBtn.addEventListener("click", sideSize());
+sizeBtn.addEventListener("click", () => createGrid());
